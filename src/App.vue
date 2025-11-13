4edd1b5a02d98ee2861c2bc6b980b65139c217cc
LAPTOP-IU4DTJF8\Nutzer <camaraousman776@gmail.com>
2025-11-13 18:02:57 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+
+
+<template>
+  <header>
+
+    <div class="wrapper">
+    <h1>Lets Start</h1>
+    </div>
+  </header>
+
+  <main>
+        <h1>Coming Soon.....</h1>
+
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  background-color: #f5f5f5;
+  padding: 20px 0;
+  text-align: center;
+
+}
+
+main {
+  padding: 40px;
+  text-align: center;
+  
+}
+
+
+
+</style>
